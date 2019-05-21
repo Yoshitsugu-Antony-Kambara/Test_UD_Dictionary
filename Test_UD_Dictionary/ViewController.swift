c7b5950434ad72718418b844a731a8ff6e92dff7
Yoshitsugu Kambara <tsugu1999213@gmail.com>
2019-05-21 16:42:09 +0900

    Initial Commit

---
+++ Test_UD_Dictionary/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Test_UD_Dictionary
+//
+//  Created by 神原良継 on 2019/05/21.
+//  Copyright © 2019 YoshitsuguKambara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var userDefaults = UserDefaults.standard
+    
+    var dic: Dictionary = ["2019/05/18": "1001"]
+
+    @IBOutlet var label: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    //button01をタップするとdictionaryにデータが以下のように追加される
+    @IBAction func button01(_ sender: Any) {
+        dic["2019/05/19"] = "1002"
+        dic["2019/05/20"] = "1003"
+        dic["2019/05/21"] = "1004"
+        
+        userDefaults.set(dic, forKey: "ratio")
+    }
+    
+    //buttonGetをタップするとラベルにデータを表示したい
+    @IBAction func buttonGet(_ sender: Any) {
+        var getData = userDefaults.dictionary(forKey: "ratio")
+        //label.text = dic["2019/05/20"]
+    }
+}
+
